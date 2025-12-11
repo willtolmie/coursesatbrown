c1455ce97c3e3031cd9b4e33a6f6bb85dff02b93
“Will” <william_tolmie@brown.edu>
2025-12-11 12:37:12 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/coursesatbrown/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
